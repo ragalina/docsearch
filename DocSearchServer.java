0cf403cba0c39d78cb3c6eb748489b9cecd526a7
Ragalina Palaka <ragalinapalaka@gmail.com>
2022-11-15 12:49:55 -0800

    Update DocSearchServer.java

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -36,6 +36,28 @@ class Handler implements URLHandler {
       this.files = FileHelpers.getFiles(Paths.get(directory));
     }
     public String handleRequest(URI url) throws IOException {
+        if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                strings.add(parameters[1]);
+                return (String.format(parameters[1] + " has been added to the ArrayList!"));
+            }
+            return "";
+        } else if (url.getPath().contains("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            String returnSearch = "This search returns: ";
+            if (parameters[0].equals("s")) {
+                String term = parameters[1];
+                for (String str : strings) {
+                    if (str.contains(term)) {
+                        returnSearch += str + ", ";
+                    }
+                }
+            }
+            return returnSearch;
+        } else {
+            return "404 Not Found!";
+        }
       return "Don't know how to handle that path!";
     }
 }
